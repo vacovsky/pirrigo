a69a960f03445f1e889cb9f17c5272e99a743886
Joe Vacovsky <vacovsky@gmail.com>
2017-02-19 23:12:30 -0800

    wired up history

--- src/pirri/pirriWebManager.go
+++ src/pirri/pirriWebManager.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	//	"fmt"
-	//	"io"
-	//	"io/ioutil"
-	//	"log"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
-func stationRun(rw http.ResponseWriter, req *http.Request) {
+func stationRunWeb(rw http.ResponseWriter, req *http.Request) {
 	var t Task = Task{Station: Station{}, StationSchedule: StationSchedule{}}
 	var msr ManualStationRun
 	ERR = json.NewDecoder(req.Body).Decode(&msr)
@@ -26,4 +24,47 @@ func stationRun(rw http.ResponseWriter, req *http.Request) {
 		spew.Dump(msr)
 	}
 	t.Send()
+}
+
+func stationAllWeb(rw http.ResponseWriter, req *http.Request) {
+	stations := []Station{}
+
+	GormDbConnect()
+	defer db.Close()
+
+	db.Limit(100).Find(&stations)
+	blob, err := json.Marshal(&stations)
+	if err != nil {
+		fmt.Println(err, err.Error())
+	}
+	io.WriteString(rw, "{ \"stations\": "+string(blob)+"}")
+}
+
+func stationGetWeb(rw http.ResponseWriter, req *http.Request) {
+	var station Station
+	ERR = json.NewDecoder(req.Body).Decode(&station)
+
+	GormDbConnect()
+	defer db.Close()
+
+	db.Where("id = ?")
+	blob, err := json.Marshal(&station)
+	if err != nil {
+		fmt.Println(err, err.Error())
+	}
+	io.WriteString(rw, string(blob))
+}
+
+func historyAllWeb(rw http.ResponseWriter, req *http.Request) {
+	history := []StationHistory{}
+
+	GormDbConnect()
+	defer db.Close()
+
+	db.Find(&history)
+	blob, err := json.Marshal(&history)
+	if err != nil {
+		fmt.Println(err, err.Error())
+	}
+	io.WriteString(rw, "{ \"history\": "+string(blob)+"}")
 }
