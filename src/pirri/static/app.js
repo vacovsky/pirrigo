ba53dade37f1bf5c52956fd29ed5fa9e312fbc8f
Joe Vacovsky <vacovsky@gmail.com>
2017-02-24 12:52:18 -0800

    calendar ui works but no data yet

--- src/pirri/static/app.js
+++ src/pirri/static/app.js
@@ -1,17 +1,26 @@
 (function() {
 
-    var app = angular.module('pirriweb', ['chart.js', 'ngCookies', 'ngMessages', 'ngMaterial', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']); //
-    app.Root = '/';
-    app.config(['$interpolateProvider',
-        function($interpolateProvider) {
-            $interpolateProvider.startSymbol('{[');
-            $interpolateProvider.endSymbol(']}');
-        }
+    var app = angular.module('pirriweb', [
+        'chart.js',
+        'ngCookies',
+        'ngMessages',
+        'ngMaterial',
+        'ngAnimate',
+        'ngSanitize',
+        'ui.calendar',
+        'ui.bootstrap'
     ]);
+    app.Root = '/';
+    // app.config(['$interpolateProvider',
+    //     function($interpolateProvider) {
+    //         $interpolateProvider.startSymbol('{[');
+    //         $interpolateProvider.endSymbol(']}');
+    //     }
+    // ]);
 
-    app.controller('PirriControl', function($rootScope, $scope, $http, $timeout, $filter, $cookies) {
+    app.controller('PirriControl', function($rootScope, $scope, $http, $timeout, $filter, $cookies, $scope, $compile, $timeout, uiCalendarConfig) {
         $rootScope.updateInterval = 6000;
-
+        $scope.eventSources = [];
         $scope.chartData1 = {
             labels: [],
             series: [],
@@ -536,7 +545,149 @@
         // };
         //$scope.intervalFunction();
 
-        this.autoLoader();
+        // START CAL
 
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $scope.events = [{
+            title: 'All Day Event',
+            start: new Date(y, m, 1),
+            color: 'orange'
+        }, {
+            title: 'Long Event',
+            start: new Date(y, m, d - 5),
+            end: new Date(y, m, d - 2),
+            color: 'blue'
+        }, {
+            id: 999,
+            title: 'Repeating Event',
+            start: new Date(y, m, d - 3, 16, 0),
+            allDay: false,
+            color: 'green'
+        }, {
+            id: 999,
+            title: 'Repeating Event',
+            start: new Date(y, m, d + 4, 16, 0),
+            allDay: false,
+            color: 'teal'
+        }, {
+            title: 'Birthday Party',
+            start: new Date(y, m, d + 1, 19, 0),
+            end: new Date(y, m, d + 1, 22, 30),
+            allDay: false,
+            color: 'pink'
+        }, {
+            title: 'Click for Google',
+            start: new Date(y, m, 28),
+            end: new Date(y, m, 29),
+            url: 'http://google.com/',
+            backgroundColor: "purple",
+            color: 'purple'
+        }];
+        /* event source that calls a function on every view switch */
+        // $scope.eventsF = function(start, end, timezone, callback) {
+        //     var s = new Date(start).getTime() / 1000;
+        //     var e = new Date(end).getTime() / 1000;
+        //     var m = new Date(start).getMonth();
+        //     var events = [{
+        //         title: 'Feed Me ' + m,
+        //         start: s + (50000),
+        //         end: s + (100000),
+        //         allDay: false,
+        //         className: ['customFeed']
+        //     }];
+        //     callback(events);
+        // };
+
+
+        /* alert on eventClick */
+        $scope.alertOnEventClick = function(date, jsEvent, view) {
+            $scope.alertMessage = (date.title + ' was clicked ');
+        };
+        /* alert on Drop */
+        $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view) {
+            $scope.alertMessage = ('Event Dropped to make dayDelta ' + delta);
+        };
+        /* alert on Resize */
+        $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view) {
+            $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+        };
+        /* add and removes an event source of choice */
+        $scope.addRemoveEventSource = function(sources, source) {
+            var canAdd = 0;
+            angular.forEach(sources, function(value, key) {
+                if (sources[key] === source) {
+                    sources.splice(key, 1);
+                    canAdd = 1;
+                }
+            });
+            if (canAdd === 0) {
+                sources.push(source);
+            }
+        };
+        /* add custom event*/
+        $scope.addEvent = function() {
+            $scope.events.push({
+                title: 'Open Sesame',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29),
+                className: ['openSesame']
+            });
+        };
+        /* remove event */
+        $scope.remove = function(index) {
+            $scope.events.splice(index, 1);
+        };
+        /* Change View */
+        $scope.changeView = function(view, calendar) {
+            uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
+        };
+        /* Change View */
+        $scope.renderCalendar = function(calendar) {
+            $timeout(function() {
+                if (uiCalendarConfig.calendars[calendar]) {
+                    uiCalendarConfig.calendars[calendar].fullCalendar('render');
+                }
+            });
+        };
+        /* Render Tooltip */
+        $scope.eventRender = function(event, element, view) {
+            element.attr({
+                'tooltip': event.title,
+                'tooltip-append-to-body': true
+            });
+            $compile(element)($scope);
+        };
+
+        /* config object */
+        $scope.uiConfig = {
+            calendar: {
+                height: 450,
+                editable: true,
+                header: {
+                    left: 'title',
+                    center: '',
+                    right: 'today prev,next'
+                },
+                eventClick: $scope.alertOnEventClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize,
+                eventRender: $scope.eventRender
+            }
+        };
+
+        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        $scope.eventSources = [$scope.events]
+
+
+        this.autoLoader();
     });
+
+    // END CAL
+
 })();
