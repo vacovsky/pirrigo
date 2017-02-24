46d7905dc7f7e625cda1705b39ac1908a2978559
Joe Vacovsky <vacovsky@gmail.com>
2017-02-23 20:30:38 -0800

    chart 2 working

--- src/pirri/pirriWebStats.go
+++ src/pirri/pirriWebStats.go
@@ -49,7 +49,7 @@ func statsActivityByDayOfWeek(rw http.ResponseWriter, req *http.Request) {
 		ReportType int
 		Labels     []string
 		Series     []string
-		Data       [][]float32
+		Data       [][]int
 	}
 
 	result := StatsChart{
@@ -62,44 +62,48 @@ func statsActivityByDayOfWeek(rw http.ResponseWriter, req *http.Request) {
 
 	type RawResult struct {
 		Day  int
-		Mins float32
+		Secs int
 	}
 
+	var rawResults0 []RawResult
 	var rawResults1 []RawResult
 	var rawResults2 []RawResult
-	var rawResults3 []RawResult
 
-	sqlQuery1 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration / 60) as mins
+	sqlQuery0 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration) as secs
             FROM station_histories
             WHERE start_time >= (CURRENT_DATE - INTERVAL ? DAY)
             GROUP BY day
             ORDER BY day ASC`)
-	sqlQuery2 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration / 60) as mins
+	sqlQuery1 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration) as secs
             FROM station_histories
             WHERE start_time >= (CURRENT_DATE - INTERVAL ? DAY) AND schedule_id > 0
             GROUP BY day
             ORDER BY day ASC`)
-	sqlQuery3 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration / 60) as mins
+	sqlQuery2 := fmt.Sprintf(`SELECT DISTINCT DAYOFWEEK((start_time + INTERVAL ? HOUR)) as day, SUM(duration) as secs
             FROM station_histories
             WHERE start_time >= (CURRENT_DATE - INTERVAL ? DAY) AND schedule_id = 0
             GROUP BY day
             ORDER BY day ASC`)
-	db.Raw(sqlQuery1, -8, 7).Scan(&rawResults1)
-	db.Raw(sqlQuery2, -8, 7).Scan(&rawResults2)
-	db.Raw(sqlQuery3, -8, 7).Scan(&rawResults3)
 
-	result.Data = [][]float32{
-		[]float32{0, 0, 0, 0, 0, 0, 0},
-		[]float32{0, 0, 0, 0, 0, 0, 0},
-		[]float32{0, 0, 0, 0, 0, 0, 0},
+	db.Raw(sqlQuery0, SETTINGS.UtcOffset, 7).Scan(&rawResults0)
+	db.Raw(sqlQuery1, SETTINGS.UtcOffset, 7).Scan(&rawResults1)
+	db.Raw(sqlQuery2, SETTINGS.UtcOffset, 7).Scan(&rawResults2)
+
+	result.Data = [][]int{
+		[]int{0, 0, 0, 0, 0, 0, 0},
+		[]int{0, 0, 0, 0, 0, 0, 0},
+		[]int{0, 0, 0, 0, 0, 0, 0},
 	}
 
-	//	for _, _ := range rawResults1 {
-	//				for result.Data[0][result.Data[len(result.Data)-1] != v.Day - 1 {
-	//					result.Data[0] = append(result.Data, v.Day - 1)
-	//				}
-
-	//	}
+	for _, v := range rawResults0 {
+		result.Data[0][v.Day] = v.Secs
+	}
+	for _, v := range rawResults1 {
+		result.Data[1][v.Day] = v.Secs
+	}
+	for _, v := range rawResults2 {
+		result.Data[2][v.Day] = v.Secs
+	}
 
 	if SETTINGS.PirriDebug {
 		//		spew.Dump(rawResults1)
@@ -171,12 +175,12 @@ func statsStationActivity(rw http.ResponseWriter, req *http.Request) {
 	result.Data = [][]int{}
 
 	sqlStr := fmt.Sprintf(`SELECT stations.id, 
-					  HOUR(start_time - INTERVAL %s HOUR) as hour, 
+					  HOUR(start_time + INTERVAL %s HOUR) as hour, 
 					  (duration) as run_secs
 				FROM station_histories
 				JOIN stations ON stations.id = station_histories.station_id
 				WHERE start_time >= (CURRENT_DATE - INTERVAL 7 DAY) 
-				ORDER BY station_id ASC`, strconv.Itoa(-1*SETTINGS.UtcOffset))
+				ORDER BY station_id ASC`, strconv.Itoa(SETTINGS.UtcOffset))
 
 	GormDbConnect()
 	defer db.Close()
