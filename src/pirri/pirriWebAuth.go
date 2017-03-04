5093d8915db440cad3160f7527de963eef872b46
Joe Vacovsky <vacovsky@gmail.com>
2017-03-03 22:54:22 -0800

    basic auth setup up for cookies AND header; added login form; probably other stuff but im 6 beers deep

--- src/pirri/pirriWebAuth.go
+++ src/pirri/pirriWebAuth.go
@@ -3,38 +3,63 @@ package main
 import (
 	"encoding/base64"
 	"net/http"
+	"net/url"
 	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
-// Leverages nemo's answer in http://stackoverflow.com/a/21937924/556573
+func loginCheck(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Congrats, you're logged in!", 200)
+}
+
+func webHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func loginAuth(rw http.ResponseWriter, req *http.Request) {
+	http.ServeFile(rw, req, "templates/login.html")
+}
+
+// Leverages nemo's answer in http://stackoverflow.com/a/21937924/556573, modified to also check cookie for auth stuff
 func basicAuth(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+		spew.Dump(r.Header.Get("Authorization"))
 
 		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 		if len(s) != 2 {
-			http.Error(w, "Not authorized", 401)
-			return
-		}
 
+			// try cookie auth!
+			c, err := r.Cookie("Authorization")
+			q, err := url.ParseQuery(c.Value)
+			for k, _ := range q {
+				s = strings.SplitN(k, " ", 2)
+			}
+			if len(s) != 2 || err != nil {
+				http.Error(w, err.Error(), 401)
+				return
+			}
+		}
 		b, err := base64.StdEncoding.DecodeString(s[1])
 		if err != nil {
 			http.Error(w, err.Error(), 401)
+			// loginAuth(w, r)
 			return
 		}
 
 		pair := strings.SplitN(string(b), ":", 2)
 		if len(pair) != 2 {
 			http.Error(w, "Not authorized", 401)
+			// loginAuth(w, r)
 			return
 		}
 
 		if pair[0] != SETTINGS.WebUser && pair[1] != SETTINGS.WebPassword {
 			http.Error(w, "Not authorized", 401)
+			// loginAuth(w, r)
 			return
 		}
-
 		h.ServeHTTP(w, r)
 	}
 }
---
+++ src/pirri/static/login.js
@@ -0,0 +1,35 @@
+(function() {
+
+    var app = angular.module('pirrilogin', [
+        'chart.js',
+        'ngCookies',
+        'base64'
+    ]);
+    app.Root = '/';
+
+    app.controller('PirriLoginControl', function($scope, $rootScope, $http, $base64, $cookies, $window) {
+
+        $scope.username = undefined;
+        $scope.password = undefined;
+
+        this.encodeForAuth = function(username, password) {
+            return $base64.encode(username + ":" + password);
+        };
+
+        this.Login = function(username, password) {
+            var encoded = this.encodeForAuth(username, password);
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + encoded;
+
+            $http.post('/login/verify')
+                .then(function(response) {
+                    console.log(response);
+                    if (response.status === 200) {
+                        $cookies.put('Authorization', $http.defaults.headers.common['Authorization']);
+                        $window.location.href = '/home';
+                    } else {
+                        alert('Incorrect login credentials...');
+                    }
+                });
+        };
+    });
+})();
