228cef6da0fba1ea0bc0b67e48984ad687ca7915
jvacovsk@calpoly.edu <jvacovsk@calpoly.edu>
2018-05-06 07:43:15 -0700

    modified sql connection logic

--- src/data/GormHelper.go
+++ src/data/GormHelper.go
@@ -1,14 +1,19 @@
 package data
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
 	"sync"
 	"time"
 
-	"../logging"
 	"../settings"
+
+	"github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"go.uber.org/zap"
 )
 
 type ORM struct {
@@ -31,32 +36,89 @@ func Service() *ORM {
 }
 
 func (d *ORM) connect() {
-	log := logging.Service()
+
+	// log := logging.Service()
 	set := settings.Service()
 	var err error
+	// if set.SQL.UseSSL {
+	// SetSSL(set.SQL.KeyPath, set.SQL.CAPath, set.SQL.CertPath)
+	// tlsProp := set.SQL.ConnectionString + "&tls=enabled"
+	// log.Println(tlsProp)
+	// d.DB, err = gorm.Open(set.SQL.DBType, tlsProp)
+	// } else {
 	d.DB, err = gorm.Open(set.SQL.DBType, set.SQL.ConnectionString)
+	// }
 	d.DB.LogMode(set.Debug.GORM)
 	if err != nil {
-		log.LogError("Unable to connect to SQL.  Trying again in 15 seconds.",
-			zap.String("DB.ype", set.SQL.DBType),
-			zap.String("connectionString", set.SQL.ConnectionString),
-			zap.String("error", err.Error()))
+		log.Println("Unable to connect to database: ", err)
+		// log.LogError("Unable to connect to SQL.  Trying again in 15 seconds.",
+		// 	zap.String("DB.ype", set.SQL.DBType),
+		// 	zap.String("connectionString", set.SQL.ConnectionString),
+		// 	zap.String("error", err.Error()))
 		for d.DB == nil {
 			time.Sleep(time.Duration(15) * time.Second)
 			d.DB, err = gorm.Open(set.SQL.DBType, set.SQL.ConnectionString)
-			log.LogError("Unable to connect to SQL on second attempt.  Fatal?  Probably.",
-				zap.String("DB.ype", set.SQL.DBType),
-				zap.String("connectionString", set.SQL.ConnectionString),
-				zap.String("error", err.Error()))
+			log.Println("Unable to connect to database a second time...  Check your configuration", err)
+
+			// log.LogError("Unable to connect to SQL on second attempt.  Fatal?  Probably.",
+			// 	zap.String("DB.type", set.SQL.DBType),
+			// 	zap.String("connectionString", set.SQL.ConnectionString),
+			// 	zap.String("error", err.Error()))
 		}
 	}
 	err = d.DB.DB().Ping()
 	if err != nil {
-		log.LogError("Ping against SQL database failed.",
-			zap.String("error", err.Error()))
+		// log.LogError("Ping against SQL database failed.",
+		// 	zap.String("error", err.Error()))
 	}
+
+	// log := logging.Service()
+	// set := settings.Service()
+	// var err error
+	// d.DB, err = gorm.Open(set.SQL.DBType, set.SQL.ConnectionString)
+	// d.DB.LogMode(set.Debug.GORM)
+	// if err != nil {
+	// log.LogError("Unable to connect to SQL.  Trying again in 15 seconds.",
+	// zap.String("DB.ype", set.SQL.DBType),
+	// zap.String("connectionString", set.SQL.ConnectionString),
+	// zap.String("error", err.Error()))
+	// for d.DB == nil {
+	// time.Sleep(time.Duration(15) * time.Second)
+	// d.DB, err = gorm.Open(set.SQL.DBType, set.SQL.ConnectionString)
+	// log.LogError("Unable to connect to SQL on second attempt.  Fatal?  Probably.",
+	// zap.String("DB.ype", set.SQL.DBType),
+	// zap.String("connectionString", set.SQL.ConnectionString),
+	// zap.String("error", err.Error()))
+	// }
+	// }
+	// err = d.DB.DB().Ping()
+	// if err != nil {
+	// log.LogError("Ping against SQL database failed.",
+	// zap.String("error", err.Error()))
+	// }
 }
 
+func SetSSL(keyPath, caPath, certPath string) {
+	rootCertPool := x509.NewCertPool()
+	pem, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+		log.Fatal("Failed to append PEM.")
+	}
+	clientCert := make([]tls.Certificate, 0, 1)
+	certs, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientCert = append(clientCert, certs)
+	mysql.RegisterTLSConfig("enabled", &tls.Config{
+		RootCAs:            rootCertPool,
+		Certificates:       clientCert,
+		InsecureSkipVerify: true,
+	})
+}
 func (d *ORM) init() {
 	d.connect()
 
