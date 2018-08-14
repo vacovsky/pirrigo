7dd0f4b2a55eba1fa6e755b3aeb8d91344160e44
jvacovsk@calpoly.edu <jvacovsk@calpoly.edu>
2018-08-14 13:43:07 -0700

    added calendar thing

--- src/webui/src/app/app.module.ts
+++ src/webui/src/app/app.module.ts
@@ -13,53 +13,70 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { FormControl } from "@angular/forms";
 
 import {
-    MatSidenavModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatListModule,
-    MatCardModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatOptionModule,
-    MatSelectModule,
-    // MatAutocompleteModule,
-    MatInputModule,
-    MatRadioModule,
-    MatProgressSpinnerModule,
-    MatExpansionModule,
-    MatDialogModule,
-    MatDialogRef
+  MatSidenavModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatCardModule,
+  MatGridListModule,
+  MatButtonModule,
+  MatOptionModule,
+  MatSelectModule,
+  // MatAutocompleteModule,
+  MatInputModule,
+  MatRadioModule,
+  MatProgressSpinnerModule,
+  MatExpansionModule,
+  MatDialogModule,
+  MatDialogRef
 } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ZonesComponent } from './components/zones/zones.component';
+import { ZonesComponent } from "./components/zones/zones.component";
 import { SidenavService } from "./services/sidenav.service";
 import { CookieService } from "ngx-cookie-service";
+import { PirriCalendarComponent } from "./components/calendar/calendar.component";
+import { FullCalendarModule } from "ng-fullcalendar";
 
 @NgModule({
-    declarations: [AppComponent, SidenavComponent, ScheduleComponent, ZonesComponent],
-    imports: [
-        MatProgressSpinnerModule,
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatListModule,
-        MatSelectModule,
-        MatCardModule,
-        HttpClientModule,
-        MatGridListModule,
-        MatExpansionModule,
-        MatButtonModule,
-        MatIconModule,
-        MatRadioModule,
-        MatFormFieldModule,
-        MatOptionModule,
-        MatSelectModule,
-        MatInputModule,
-        MatDialogModule
-    ],
-    providers: [Globals, HttpClient, MediaMatcher, StationService, SidenavService, CookieService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+    ScheduleComponent,
+    ZonesComponent,
+    PirriCalendarComponent
+  ],
+  imports: [
+    MatProgressSpinnerModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSelectModule,
+    MatCardModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDialogModule,
+    BrowserModule,
+    FullCalendarModule
+  ],
+  providers: [
+    Globals,
+    HttpClient,
+    MediaMatcher,
+    StationService,
+    SidenavService,
+    CookieService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
