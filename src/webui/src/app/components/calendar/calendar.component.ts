5b0cdd6e77da8cd395d6a91240674a7031429710
Joe Vacovsky <vacovsky@gmail.com>
2018-08-15 20:41:35 -0700

    remove calendar junk

--- src/webui/src/app/components/calendar/calendar.component.ts
+++ src/webui/src/app/components/calendar/calendar.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CalendarComponent } from 'ng-fullcalendar';
-import { Options } from 'fullcalendar';
+// import { CalendarComponent } from 'ng-fullcalendar';
+// import { Options } from 'fullcalendar';
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+    selector: 'app-calendar',
+    templateUrl: './calendar.component.html',
+    styleUrls: ['./calendar.component.css']
 })
 export class PirriCalendarComponent implements OnInit {
 
-  calendarOptions: Options;
-  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
-  constructor() {}
-  ngOnInit() {
-     this.calendarOptions = {
-        editable: true,
-        eventLimit: false,
-        header: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'month,agendaWeek,agendaDay,listMonth'
-        },
-        events: []
-      };
-  }
+    // calendarOptions: Options;
+    // @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
+    constructor() {}
+    ngOnInit() {
+        // this.calendarOptions = {
+        // editable: true,
+        // eventLimit: false,
+        // header: {
+        // left: 'prev,next today',
+        // center: 'title',
+        // right: 'month,agendaWeek,agendaDay,listMonth'
+        // },
+        // events: []
+        // };
+    }
 
 }
